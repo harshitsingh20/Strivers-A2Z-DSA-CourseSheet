f115dd776699ec6053a71cdf7debe3ca17fa0886
harshitsingh20 <harshitsingh2035@gmail.com>
2023-05-02 22:14:07 +0530

    User Input

---
+++ Step 1 ( Learn the basics )/Step 1.1 (Things to Know in Python Programming Language )/UserInputAndOutput.py
@@ -0,0 +1,20 @@
+# User Input
+
+# Examples
+
+# Taking input from the user (It's takes input as a String)
+n = input("Enter your n: ")
+
+# Output
+print("Hello, " + n)
+
+# type is the built-in function which tells which type of data type we used
+print(type(n))
+
+
+# Taking input from the user (It's takes input as a Number)
+a = int(input("Enter your number: "))
+
+# Output
+print("Number is, " , a)
+print(type(a))
